6cd115acec0dc69a5c3b149c4e6b39ed6a9cbaf1
Karin Huber <karin@software-architects.at>
2022-12-17 09:25:40 +0100

    day 17

---
+++ 2022/17/index.spec.ts
@@ -0,0 +1,20 @@
+import { part1, part2 } from './solve';
+const jestConsole = console;
+beforeEach(() => (global.console = require('console')));
+afterEach(() => (global.console = jestConsole));
+
+describe('Day 2022/01', () => {
+  describe('Part 1', () => {
+    it('test 1', () => {
+      expect(part1(input1.value)).toEqual(input1.result1);
+    });
+  });
+
+  describe('Part 2', () => {
+    it('test 1', () => {
+      expect(part2(input1.value)).toEqual(input1.result2);
+    });
+  });
+});
+
+const input1 = { value: `>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>`, result1: 3068, result2: 1514285714288 };
---
+++ 2022/17/solve.ts
@@ -0,0 +1,225 @@
+import { logError, logHighlight, logSuccess } from '../../functions/log';
+import { writeFileSync } from 'fs';
+
+export function part1(input: string): string | number {
+  let jetIndex = 0;
+  const tower: string[] = [];
+  const rocks: string[][] = [];
+
+  rocks.push(['..####.']);
+  rocks.push(['...#...', '..###..', '...#...']);
+  rocks.push(['....#..', '....#..', '..###..']);
+  rocks.push(['..#....', '..#....', '..#....', '..#....']);
+  rocks.push(['..##...', '..##...']);
+
+  for (let i = 0; i < 640; i++) {
+    let rock = rocks[i % rocks.length].slice();
+    let pos = tower.length + 3;
+    let canFall = true;
+
+    while (canFall) {
+      // jet
+      rock = applyJet(tower, rock, input[jetIndex % input.length], pos);
+
+      // fall
+      canFall = canMoveDown(tower, rock, pos);
+
+      // add to tower
+      if (!canFall) {
+        addToTower(tower, rock, pos);
+      }
+
+      pos--;
+      jetIndex++;
+    }
+
+    console.log(i + 1, tower.length);
+    if (i + 1 === 627) {
+      writeFileSync('./2022/17/output.txt', tower.reverse().join('\n'));
+      console.log(tower.slice(900).reverse().join('\n'));
+    }
+  }
+
+  //console.log(tower.slice(900).reverse().join('\n'));
+
+  return tower.length;
+}
+
+export function part2(input: string): string | number {
+  return -1;
+  let jetIndex = 0;
+  const tower: string[] = [];
+  const rocks: string[][] = [];
+  const added: number[] = [];
+
+  rocks.push(['..####.']);
+  rocks.push(['...#...', '..###..', '...#...']);
+  rocks.push(['....#..', '....#..', '..###..']);
+  rocks.push(['..#....', '..#....', '..#....', '..#....']);
+  rocks.push(['..##...', '..##...']);
+
+  let prevTowerLength = 0;
+  let maxRocks = 1000000000000;
+  let repeatHeight = 0;
+  let repeatLength = 0;
+  let minRepeatLength = 100;
+
+  for (let i = 0; i < maxRocks; i++) {
+    let rock = rocks[i % rocks.length].slice();
+    let pos = tower.length + 3;
+    let canFall = true;
+
+    while (canFall) {
+      // jet
+      rock = applyJet(tower, rock, input[jetIndex % input.length], pos);
+
+      // fall
+      canFall = canMoveDown(tower, rock, pos);
+
+      // add to tower
+      if (!canFall) {
+        addToTower(tower, rock, pos);
+      }
+
+      pos--;
+      jetIndex++;
+    }
+
+    added.push(tower.length - prevTowerLength);
+    prevTowerLength = tower.length;
+
+    if (i % 1000 === 0) {
+      console.log(i);
+    }
+
+    // find repeating pattern
+    if (added.length > 10000 && !repeatHeight) {
+      for (let length = 100; length <= added.length / 2; length++) {
+        const last = added.slice(added.length - length);
+        const prev = added.slice(added.length - length - length, added.length - length);
+
+        if (last.map((x) => x.toString()).join(',') === prev.map((x) => x.toString()).join(',')) {
+          const height = last.reduce((prev, curr) => prev + curr, 0);
+          repeatHeight = height;
+          repeatLength = last.length;
+          maxRocks = i + ((maxRocks - i) % repeatLength);
+
+          logSuccess(
+            'REPEATING SECTION',
+            i,
+            'tower height',
+            tower.length,
+            'repeat length',
+            last.length,
+            'repeat height',
+            height,
+            'max rocks',
+            maxRocks
+          );
+        }
+      }
+    }
+  }
+
+  logSuccess(
+    'RESULT',
+    maxRocks,
+    'tower height',
+    tower.length,
+    'repeat length',
+    repeatLength,
+    'repeat height',
+    repeatHeight
+  );
+
+  //console.log(added.map((x) => x.toString()).join(','));
+
+  return tower.length + repeatHeight * ((1000000000000 - maxRocks) / repeatLength);
+}
+
+function addToTower(tower: string[], rock: string[], pos: number) {
+  for (let i = 0; i < rock.length; i++) {
+    const towerPos = pos + (rock.length - 1 - i);
+
+    if (!tower[towerPos]) {
+      tower[towerPos] = rock[i];
+    } else {
+      let newRow = '';
+      for (let j = 0; j < tower[0].length; j++) {
+        if (rock[i][j] === '#') {
+          newRow += '#';
+        } else {
+          newRow += tower[towerPos][j];
+        }
+      }
+      tower[towerPos] = newRow;
+    }
+  }
+
+  // logHighlight('addToTower');
+  // console.log(tower.slice().reverse().map(s => '|' + s + '|').join('\n'));
+}
+
+function canMoveDown(tower: string[], rock: string[], pos: number): boolean {
+  let canMove = pos > 0;
+
+  for (let i = 0; i < rock.length && canMove; i++) {
+    const towerRow = tower[pos - 1 + (rock.length - 1 - i)];
+    if (towerRow) {
+      for (let j = 0; j < tower[0].length; j++) {
+        if (rock[i][j] === '#' && towerRow[j] === '#') {
+          canMove = false;
+        }
+      }
+    }
+  }
+
+  return canMove;
+}
+
+function applyJet(tower: string[], rock: string[], jet: string, pos: number): string[] {
+  let canMove = true;
+  for (let i = 0; i < rock.length && canMove; i++) {
+    if (jet === '>') {
+      if (rock[i].endsWith('#')) {
+        canMove = false;
+      }
+    } else if (jet === '<') {
+      if (rock[i].startsWith('#')) {
+        canMove = false;
+      }
+    }
+
+    // check tower
+    const towerRow = tower[pos + (rock.length - 1 - i)];
+    if (towerRow) {
+      if (jet === '>') {
+        for (let j = 1; j < tower[0].length; j++) {
+          if (rock[i][j - 1] === '#' && towerRow[j] === '#') {
+            canMove = false;
+          }
+        }
+      } else {
+        for (let j = 0; j < tower[0].length - 1; j++) {
+          if (rock[i][j + 1] === '#' && towerRow[j] === '#') {
+            canMove = false;
+          }
+        }
+      }
+    }
+  }
+
+  if (canMove) {
+    for (let i = 0; i < rock.length; i++) {
+      if (jet === '>') {
+        rock[i] = '.' + rock[i].substring(0, rock[i].length - 1);
+      } else if (jet === '<') {
+        rock[i] = rock[i].substring(1) + '.';
+      }
+    }
+  }
+
+  //console.log('applyJet', rock, pos);
+
+  return rock;
+}
